99bf7121a7ecbe5c061f95a33b4d441a8115ced4
isagrt <isadoragoedert05@gmail.com>
2022-08-22 08:30:48 -0300

    começo projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,67 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      qualidades: [
+        { qualidade: 5, servico: "5% (Péssimo)" },
+        { qualidade: 10, servico: "10% (Ruim)" },
+        { qualidade: 15, servico: "15% (OK)" },
+        { qualidade: 20, servico: "20% (Bom)" },
+        { qualidade: 25, servico: "25% (Mais que bom)" },
+        { qualidade: 30, servico: "30% (Exelente)" },
+      ],
+      valor: 0,
+      pessoa: 1,
+      qualidade_selecionada: "",
+    };
+  },
+  computed: {
+    valor_final() {
+      return (
+        (this.valor * (1 + this.qualidade_selecionada / 100)) /
+        this.pessoa
+      ).toFixed(2);
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div>
+      <h1>Calculadora de Gorjetas</h1>
+      <div class="forms-gorjeta">
+        <label for="">Qual o valor da conta?</label>
+        <input type="number" v-model="valor" placeholder="0" />
+      </div>
+      <div class="forms-gorjeta">
+        <label for="select-servico">Como foi o serviço?</label>
+        <select
+          name="select-qualidade "
+          id="select-servico"
+          v-model="qualidade_selecionada"
+        >
+          <option disabled>Escolha um item</option>
+          <option
+            v-for="qualidade of qualidades"
+            :key="qualidade.qualidade"
+            for="select-servico"
+            :value="qualidade.qualidade"
+          >
+            {{ qualidade.servico }}
+          </option>
+        </select>
+      </div>
+
+      <div class="forms-gorjeta">
+        <label for="">Quantas pessoas iram pagar?</label>
+        <input type="number" v-model="pessoa" placeholder="1" />
+      </div>
+
+      <div>
+        <h2>Valor da conta:</h2>
+        <span>R$ {{ valor_final }} por pessoa</span>
+      </div>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
